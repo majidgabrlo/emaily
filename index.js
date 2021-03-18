8b95322029372a85cab7206ab79199194fa5f0dc
unknown <majidgabrlo@gmail.com>
2021-03-18 14:48:26 +0330

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'Hello' });
+  res.send({ hi: 'Hello pal' });
 });
 
 const PORT = process.env.PORT || 5000;
